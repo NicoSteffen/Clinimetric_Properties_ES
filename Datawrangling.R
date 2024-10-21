09d4a1546532afccd7f8b33293df13565fc4dd62
NicoSteffen <nico@die-steffens.de>
2024-10-21 13:53:32 +0200

    update

---
+++ Datawrangling.R
@@ -0,0 +1,14 @@
+library(foreign)
+library(tidyverse)
+library(easystats)
+library(lme4)
+library(lmerTest)
+library(dendextend)
+library(lavaan)
+library(tidyr)
+library(dplyr)
+library(moments)
+library(psych)
+
+options(scipen = 999)
+
