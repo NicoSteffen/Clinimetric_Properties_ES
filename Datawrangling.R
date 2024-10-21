184a00d6b5ee59a7bd2e21f5777a349198aacd92
NicoSteffen <nico@die-steffens.de>
2024-10-21 16:05:30 +0200

    update

--- Datawrangling.R
+++ Datawrangling.R
@@ -1,3 +1,5 @@
+# Load Packages -----------------------------------------------------------
+
 library(foreign)
 library(tidyverse)
 library(easystats)
@@ -10,5 +12,225 @@ library(dplyr)
 library(moments)
 library(psych)
 
-options(scipen = 999)
+# Create custom functions -------------------------------------------------
+
+roundallnumerics = function(df, digits){
+  for(j in 1:ncol(df)){
+    if(is.numeric(df[,j])){
+      df[,j] = round(df[,j], digits)
+    }
+  }
+  return(df)
+}
+
+p_labeller = function(vec){
+  vec = as.numeric(vec)
+  for(i in 1:length(vec)){
+    if(is.na(vec[i]) == F & vec[i] < .001){
+      vec[i] = "<.001***"
+    }
+    if(is.na(vec[i]) == F & vec[i] >= .001 & vec[i] < .01){
+      vec[i] = paste0(vec[i], "**")
+    }
+    if(is.na(vec[i]) == F & vec[i] > .01 & vec[i] < .05){
+      vec[i] = paste0(vec[i], "*")
+    }
+  }
+  return(vec)
+}
+
+# Read data ---------------------------------------------------------------
+
+data = read.csv2("data.csv")
+
+
+# Data cleaning --------------------------------------------------------------
+
+colnames(data)[colnames(data) == "lfdn"] <- "id"
+
+#remove ID-1 (test run)
+data <- data[data$id != 1, ]
+
+#exclusion of somatic = 1
+data <- data[data$somatic != 1, ]
+
+#exclusion of Schwanger = 1
+data <- data[data$Schwanger != 1, ]
+
+#recode answers of ID 4 (changed coding afterwards)
+data[data$id == 4, grep("^ES_", colnames(data))] <- 
+  lapply(data[data$id == 4, grep("^ES_", colnames(data))], function(x) ifelse(x == 1, 2, ifelse(x == 2, 1, x)))
+
+
+data[data$id == 4, grep("^MINI_current_|^MINI_past_", colnames(data))] <- 
+  lapply(data[data$id == 4, grep("^MINI_", colnames(data))], function(x) ifelse(x == 1, 2, ifelse(x == 2, 1, x)))
+
+
+
+# sample characteristics --------------------------------------------------
+
+characteristics = c("sex",
+                    "age",
+                    "Beziehungsstatus",
+                    "Kinder",
+                    "Beschaeftigungsverhaeltnis",
+                    "Bildungsabschluss",
+                    "Einkommen",
+                    "Wohnsituation")
+
+# Umkodierung von 'sex' in einen Faktor
+data$sex <- factor(data$sex, levels = c(1, 2, 3), labels = c("weiblich", "männlich", "non-binär/divers"))
+
+# Umkodierung von 'Beziehungsstatus' in einen Faktor
+data$Beziehungsstatus <- factor(data$Beziehungsstatus, levels = c(1, 2, 3, 4, 5, 6, 7),
+                                labels = c("Verheiratet", "Verwitwet", "Geschieden", "Getrennt lebend", "Single",
+                                           "In einer festen Partnerschaft (zusammen lebend)", 
+                                           "In einer festen Partnerschaft (getrennt lebend)"))
+
+# Umkodierung von 'Kinder' in einen Faktor
+data$Kinder <- factor(data$Kinder, levels = c(1, 2, 3, 4, 5), 
+                      labels = c("Nein, keine Kinder", "Ja, 1 Kind", "Ja, 2 Kinder", "Ja, 3 Kinder", "Ja, 4 Kinder oder mehr"))
+
+# Umkodierung von 'Beschaeftigungsverhaeltnis' in einen Faktor
+data$Beschaeftigungsverhaeltnis <- factor(data$Beschaeftigungsverhaeltnis, levels = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11), 
+                                          labels = c("Student/in", "Angestellt (Vollzeit)", "Angestellt (Teilzeit)", 
+                                                     "Selbstständig", "Freiberuflich tätig", "Arbeitslos", 
+                                                     "In Ausbildung", "Rentner/in", "Hausfrau/Hausmann", 
+                                                     "Elternzeit", "Sonstiges Arbeitsverhältnis"))
+
+# Umkodierung von 'Bildungsabschluss' in einen Faktor
+data$Bildungsabschluss <- factor(data$Bildungsabschluss, levels = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15), 
+                                 labels = c("Keinen Schulabschluss", "Hauptschulabschluss", "Realschulabschluss", 
+                                            "Fachabitur (FOS, BOS)", "Allgemeines Abitur", "Abgeschlossene Ausbildung", 
+                                            "Hochschule (Diplom)", "Hochschule (Magister)", "Hochschule (Bachelor)", 
+                                            "Hochschule (Master)", "Hochschule (Promotion)", "Hochschule (Habilitation)", 
+                                            "Hochschule (Staatsexamen)", "Sonstiger Abschluss"))
+
+# Umkodierung von 'Einkommen' in einen Faktor
+data$Einkommen <- factor(data$Einkommen, levels = c(1, 2, 3, 4, 5, 6, 7), 
+                         labels = c("Unter 1.000 €", "1.000 - 1.999 €", "2.000 - 2.999 €", "3.000 - 3.999 €", 
+                                    "4.000 - 4.999 €", "5.000 oder mehr", "keine Angabe"))
+
+# Umkodierung von 'Wohnsituation' in einen Faktor
+data$Wohnsituation <- factor(data$Wohnsituation, levels = c(1, 2, 3, 4, 5), 
+                             labels = c("Bei meinen Eltern/Verwandten", "Eigene Wohnung/Haus (zur Miete)", 
+                                        "Eigene Wohnung/Haus (Eigentum)", "WG (zur Miete)", "WG (Eigentum)"))
+
+
+
+flextable::save_as_docx(flextable::flextable(report_sample(
+  data[, c(characteristics)],group_by = "sex")), path = "Table1.docx")
+
+
+# Skalen umkodieren + aggregation ------------------------------------------------------
+
+# ES_dichotom
+
+# Umkodierung der ES_dichotom-Variablen: 1 -> 0 ; 2 -> 1
+data[, grep("^ES_(?!likert)", colnames(data), perl = TRUE)] <- 
+  lapply(data[, grep("^ES_(?!likert)", colnames(data), perl = TRUE)],
+         function(x) ifelse(x == 1, 0, ifelse(x == 2, 1, x)))
+
+data$ES_total <- rowSums(data[, grep("^ES_[1-9]$|^ES_10$", colnames(data))], na.rm = TRUE)
+
+
+# ES_likert
+
+data$ES_likert_total = rowSums(data[, grep("ES_likert", colnames(data))], na.rm = TRUE)
+
+# BDI_
+
+# Umkodierung der allgemeinen BDI_ Variablen außer BDI_16 und BDI_18
+bdigeneral_vars <- grep("^BDI_", colnames(data), value = TRUE)
+bdigeneral_vars <- setdiff(bdigeneral_vars, c("BDI_16", "BDI_18"))
+
+data[, bdigeneral_vars] <- lapply(data[, bdigeneral_vars], function(x) {
+  ifelse(x == 1, 0, ifelse(x == 2, 1, ifelse(x == 3, 2, ifelse(x == 4, 3, x))))
+})
+
+# Spezielle Umkodierung für BDI_16 und BDI_18
+data$BDI_16 <- ifelse(data$BDI_16 == 1, 0, 
+                      ifelse(data$BDI_16 %in% c(2, 3), 1, 
+                             ifelse(data$BDI_16 %in% c(4, 5), 2, 
+                                    ifelse(data$BDI_16 %in% c(6, 7), 3, data$BDI_16))))
+
+data$BDI_18 <- ifelse(data$BDI_18 == 1, 0, 
+                      ifelse(data$BDI_18 %in% c(2, 3), 1, 
+                             ifelse(data$BDI_18 %in% c(4, 5), 2, 
+                                    ifelse(data$BDI_18 %in% c(6, 7), 3, data$BDI_18))))
+
+
+data$BDI_total = rowSums(data[, grep("BDI_", colnames(data))], na.rm = TRUE)
+
+# MINI Grouping
+
+
+data[, grep("^MINI_", colnames(data))] <- 
+  lapply(data[, grep("^MINI_", colnames(data))], function(x) ifelse(x == 1, 0, ifelse(x == 2, 1, x)))
+
+
+# Berechnung der Summe der MINI_current und MINI_past Variablen
+mini_current_sum <- rowSums(data[, grep("^MINI_current_", colnames(data))], na.rm = TRUE)
+mini_past_sum <- rowSums(data[, grep("^MINI_past_", colnames(data))], na.rm = TRUE)
+
+# Codierung für Mini_lifetime_MDE (YES/NO)
+data$Mini_lifetime_MDE <- ifelse(mini_past_sum >= 0 & mini_past_sum <= 4, "NO", 
+                                 ifelse(mini_past_sum >= 5 & mini_past_sum <= 9, "YES", NA))
+
+# Codierung für Mini_current_MDE (MDE/Subthreshold Depression/None)
+data$Mini_current_MDE <- ifelse(mini_current_sum == 0, "None", 
+                                ifelse(mini_current_sum >= 1 & mini_current_sum <= 4, "Subthreshold Depression", 
+                                       ifelse(mini_current_sum >= 5 & mini_current_sum <= 9, "MDE", NA)))
+
+# Optional: Als Faktoren kodieren
+data$Mini_lifetime_MDE <- factor(data$Mini_lifetime_MDE, levels = c("NO", "YES"))
+data$Mini_current_MDE <- factor(data$Mini_current_MDE, levels = c("None", "Subthreshold Depression", "MDE"))
+
+# Erstellung der Gruppen basierend auf Mini_current_MDE und Mini_lifetime_MDE
+data$MINI_Group <- ifelse(data$Mini_current_MDE == "None" & data$Mini_lifetime_MDE == "NO", "Group0: No history of MDE - healthy",
+                     ifelse(data$Mini_current_MDE == "None" & data$Mini_lifetime_MDE == "YES", "Group1: Full remission",
+                            ifelse(data$Mini_current_MDE == "Subthreshold Depression" & data$Mini_lifetime_MDE == "NO", "Group2: First subthreshold depressive episode",
+                                   ifelse(data$Mini_current_MDE == "Subthreshold Depression" & data$Mini_lifetime_MDE == "YES", "Group3: History of MDE + current subthreshold",
+                                          ifelse(data$Mini_current_MDE == "MDE" & data$Mini_lifetime_MDE == "YES", "Group4: History of MDE + current MDE", NA)))))
+
+# Optional: Als Faktor kodieren, um die Gruppen geordnet darzustellen
+data$MINI_Group <- factor(data$MINI_Group, levels = c("Group0: No history of MDE - healthy", 
+                                            "Group1: Full remission", 
+                                            "Group2: First subthreshold depressive episode", 
+                                            "Group3: History of MDE + current subthreshold", 
+                                            "Group4: History of MDE + current MDE"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+model = lm(ES_total ~ MINI_Group, data = data)
+anova(model)
+
+emmeans::emmeans(model, pairwise ~ MINI_Group)
+
+
+
+
+
+
+
+
+
+
+
+model = lm(BDI_total ~ ES_total, data = data)
+summary(model)
+
+model2 = lm(BDI_total ~ ES_likert_total, data = data)
+summary(model2)
+
 
